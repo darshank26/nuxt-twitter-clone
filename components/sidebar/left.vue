266eb0ca900833c42e10be1fa075090003f99eef
darshan <darsh2605@gmail.com>
2023-07-16 13:00:47 +0530

    Initial Commit

---
+++ components/sidebar/left.vue
@@ -0,0 +1,28 @@
+<template>
+
+
+<div class="flex flex-col h-screen">
+    <div class="p-2 my-2 hover:bg-blue-50 rounded-full w-min
+dark:hover:bg-white/20 " :class="transition">
+    <NuxtLink to="">
+        <div class="w-8 h-8">
+      <LogosTwitter/>
+      </div>
+    </NuxtLink>
+</div>  
+</div>
+
+
+
+
+    
+
+</template>
+
+<script setup>
+const transition = 'transition ease-in-out duration-350'
+</script>
+
+<style scoped>
+
+</style>
